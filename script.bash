c57b06730675d37593036e3eeaa186881f62fab8
Andrew <andrew.stepko@gmail.com>
2016-12-07 15:35:10 +0200

    Added comments for functions.

--- script.bash
+++ script.bash
@@ -1,3 +1,5 @@
+#!/bin/bash
+#(c) Andrii Stepko (PI-13-1)
 FIELD_SIZE=3;
 EMPTY_CELL=0;
 PLAYER_ONE_CELL=1;
@@ -13,6 +15,7 @@ set_black(){ echo -en "\e[40m";}
 
 set_default(){ echo -en "\e[0m";}
 
+# Starts a new game.
 new(){
 	field=();
 	field_length=$(($FIELD_SIZE*$FIELD_SIZE));
@@ -26,6 +29,7 @@ new(){
 	field
 }
 
+# Prints current game field into console.
 field(){
 	for ((i=0; i<$FIELD_SIZE; i++)) do
 		for ((j=0; j<$FIELD_SIZE; j++)) do
@@ -44,6 +48,7 @@ field(){
 	fi
 }
 
+# Prints a cell of a field into console. 1) cell's value.
 print_cell(){
 	if [ $1 == $EMPTY_CELL ]; then
 		set_black;
@@ -59,6 +64,7 @@ print_cell(){
 	set_default;
 }
 
+# Makes player's move. 1) row number; 2) column number.
 move(){
 	if [ $winner == 0 ]; then
 		if [ $turn == 1 ]; then
@@ -72,6 +78,7 @@ move(){
 	fi
 }
 
+# Makes move of the first player. 1) row number; 2) column number.
 first(){
 	move_forcibly $1 $2 1;
 	res=$?
@@ -82,6 +89,7 @@ first(){
 	fi
 }
 
+# Makes move of the second player. 1) row number; 2) column number.
 second(){
 	move_forcibly $1 $2 2;
 	res=$?
@@ -92,6 +100,7 @@ second(){
 	fi
 }
 
+# Makes move of a player. 1) row number; 2) column number; 3) player's number.
 move_forcibly(){
 	if (($1 >= $FIELD_SIZE)) || (($2 >= $FIELD_SIZE)); then
 		echo "Error index out of range.";
@@ -108,6 +117,7 @@ move_forcibly(){
 	return 1;
 }
 
+# Prints the winner into console, if any exists.
 echo_winners(){
 	is_some_line_full 1;
 	res=$?
@@ -126,6 +136,7 @@ echo_winners(){
 	fi
 }
 
+# Returns 1 if there is some full line in the field or 0 otherwise. 1)user number.
 is_some_line_full(){
 	for ((k=0; k<$FIELD_SIZE; k++)) do
 		is_row_full $k $1
@@ -155,6 +166,7 @@ is_some_line_full(){
 	return 0;
 }
 
+# returns 1 if particular row is full and zero otherwise. 1)row index; 2) payer's number;
 is_row_full(){
 	for ((j=0; j<$FIELD_SIZE; j++)) do
 		t=$(($1*$FIELD_SIZE+$j));
@@ -165,6 +177,7 @@ is_row_full(){
 	return 1;
 }
 
+# returns 1 if particular row is full and 0 otherwise. 1)row index; 2) payer's number;
 is_column_full(){
 	for ((i=0; i<$FIELD_SIZE; i++)) do
 		t=$(($i*$FIELD_SIZE+$1));
@@ -175,6 +188,7 @@ is_column_full(){
 	return 1;
 }
 
+# returns 1 if main diagonal is full and 0 otherwise. 1)payer's number;
 is_main_diag_full(){
 	for ((i=0; i<$FIELD_SIZE; i++)) do
 		t=$(($i*$FIELD_SIZE+$i));
@@ -185,6 +199,7 @@ is_main_diag_full(){
 	return 1;
 }
 
+# returns 1 if secondary diagonal is full and 0 otherwise. 1)payer's number;
 is_secondary_diag_full(){
 	for ((i=0; i<$FIELD_SIZE; i++)) do
 		t=$(($i*$FIELD_SIZE+$FIELD_SIZE-$i-1));
